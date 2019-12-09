7b5200927c61ad8fbb6cb682fef2c8a86bc1d050
Andras Svraka <svraka.andras@gmail.com>
2019-12-09 19:14:27 +0100

    Stick to Pandoc's citeproc
    It allows adding an appendix after the references without heavy
    modifications to the template.

--- R/aswp.R
+++ R/aswp.R
@@ -6,9 +6,6 @@
 #' @inheritParams rmarkdown::pdf_document
 #' @param ... Additional arguments to
 #'   \code{\link[bookdown:pdf_document2]{bookdown::pdf_document2()}}
-#' @param citation_package The LaTeX package to process citations,
-#'   \code{natbib} or \code{biblatex}. Use \code{none} if neither
-#'   package is to be used. This format defaults to \code{biblatex}.
 #'
 #' @details This package currently supports only XeLaTeX.
 #' @inherit rmarkdown::pdf_document return
@@ -17,8 +14,7 @@
 #' @export
 aswp <- function(...,
                  toc = FALSE,
-                 keep_tex = TRUE,
-                 citation_package = "biblatex") {
+                 keep_tex = TRUE) {
   template <- system.file("rmarkdown", "templates", "aswp", "resources", "template.tex",
                           package = "aswp")
 
@@ -26,8 +22,7 @@ aswp <- function(...,
                                 toc = toc,
                                 template = template,
                                 keep_tex = keep_tex,
-                                latex_engine = "xelatex",
-                                citation_package = citation_package)
+                                latex_engine = "xelatex")
 
   base
 }
