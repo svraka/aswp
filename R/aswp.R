8384466f4f359e35a05b32ee07bb89d919eca4b5
Andras Svraka <svraka.andras@gmail.com>
2019-12-09 17:35:06 +0100

    Add documentation

--- R/aswp.R
+++ R/aswp.R
@@ -1,3 +1,20 @@
+#' An opinionated LaTeX template for R Markdown working papers
+#'
+#' A template suitable for working paperes written in R Markdown. It
+#'   provides usefule defaults and nice typography.
+#'
+#' @inheritParams rmarkdown::pdf_document
+#' @param ... Additional arguments to
+#'   \code{\link[bookdown:pdf_document2]{bookdown::pdf_document2()}}
+#' @param citation_package The LaTeX package to process citations,
+#'   \code{natbib} or \code{biblatex}. Use \code{none} if neither
+#'   package is to be used. This format defaults to \code{biblatex}.
+#'
+#' @details This package currently supports only XeLaTeX.
+#' @inherit rmarkdown::pdf_document return
+#'
+#' @author András Svraka
+#' @export
 aswp <- function(...,
                  toc = FALSE,
                  keep_tex = TRUE,
@@ -16,7 +33,7 @@ aswp <- function(...,
 }
 
 # Call `bookdown::pdf_document2` and mark the return value as
-# inheriting `pdf_document`.
+# inheriting `pdf_document2`.
 inherit_pdf_document2 <- function(...) {
     fmt <- bookdown::pdf_document2(...)
     fmt$inherits <- "pdf_document2"
