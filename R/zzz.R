874d670f03d8caec47df9e5f319753736f5b76ae
Andras Svraka <svraka.andras@gmail.com>
2019-12-13 08:25:45 +0100

    Evaluate all caption-like options after knitting chunks.

--- R/zzz.R
+++ R/zzz.R
@@ -5,4 +5,11 @@
   # `knitr` defaults to markdown figures, so we need to tell it to use
   # LaTeX. See <https://github.com/yihui/knitr/issues/1464> and <https://ulyngs.github.io/blog/posts/2019-02-01-how-to-create-your-own-chunk-options-in-r-markdown/>
   knitr::knit_hooks$set(plot = knitr::hook_plot_tex)
+
+  # Evaluate these chunk options after knitting, so results are
+  # available for captions and notes.
+  evalq(
+    knitr::opts_knit$set(eval.after = c("fig.cap", "fig.scap", "fig.subcap", "floatfoot")),
+    envir = getNamespace("knitr")
+  )
 }
